8f1a06aade2830d28823b3da0e80880668a81e4c
Marco Hinojosa <marco.a.hinojosa@gmail.com>
2022-03-03 13:38:59 -0600

    test of realtime

--- shiny_app_first/ui.R
+++ shiny_app_first/ui.R
@@ -8,24 +8,24 @@ library(plotly)
 shinyUI(fluidPage(theme = shinytheme("superhero"),
 
     # Application title
-    titlePanel("Shiny COVID19 Dashboard"),
+    titlePanel("Test of realtime"), #"Shiny COVID19 Dashboard"),
 
     # Sidebar with inputs and outputs as below in the panel
     sidebarLayout(
         sidebarPanel(
-          selectInput('variables', "Select plot(s)", colnames(dat1)[-(1:3)], 
-                      multiple = TRUE, 
+          selectInput('variables', "Select plot(s)", colnames(dat1)[-(1:3)],
+                      multiple = TRUE,
                       selectize = TRUE,
-                      selected = colnames(dat1)[4]), 
+                      selected = colnames(dat1)[4]),
           # preloads/matches choices on typing
-          # input selector populated with column names from dat1 dataframe, specifically 
+          # input selector populated with column names from dat1 dataframe, specifically
           # all column names except the first three.
-          # the selectInput definition: function (inputId, label, choices, selected = NULL, multiple = FALSE, 
+          # the selectInput definition: function (inputId, label, choices, selected = NULL, multiple = FALSE,
           # selectize = TRUE, width = NULL, size = NULL)
-          
+
           uiOutput("ycol")),
           # outputs the "ycol" content generated in server in the sidebar
-   
+
         mainPanel(plotlyOutput("distPlot"))
     )
 ))
